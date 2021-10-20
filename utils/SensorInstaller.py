1ff1606fc1650902d58ebc61cc97b90ea40611e9
ghajgato <ghajgato@hds.bme.hu>
2021-10-20 11:26:51 +0200

    deploy by weighted shortest path length

--- utils/SensorInstaller.py
+++ utils/SensorInstaller.py
@@ -39,7 +39,7 @@ class SensorInstaller():
             raise
         return master_nodes
 
-    def random_deploy(self, sensor_budget, seed=None):
+    def deploy_by_random(self, sensor_budget, seed=None):
         num_nodes   = len(self.G.nodes)
         signal_mask = np.ones(shape=(num_nodes,), dtype=np.int8)
         if seed:
@@ -52,3 +52,24 @@ class SensorInstaller():
         signal_mask[unobserved_nodes]   = 0
         self.sensor_nodes   = set(
                 np.array(list(self.G.nodes))[np.where(signal_mask)[0]])
+
+    def deploy_by_shortest_path(self, sensor_budget, weight_by=None):
+        sensor_nodes    = set()
+        for _ in range(sensor_budget):
+            path_lengths    = dict()
+            for node in self.G.nodes:
+                path_lengths[node]  = 0
+            for node in self.master_nodes.union(sensor_nodes):
+                tempo   = nx.shortest_path_length(
+                    self.G,
+                    source  = node,
+                    weight  = weight_by
+                    )
+                for key, value in tempo.items():
+                    if key not in self.master_nodes.union(sensor_nodes):
+                        path_lengths[key]   += value
+            sensor_nodes.add(
+                    [candidate for candidate, path_length in path_lengths.items()
+                        if path_length == np.max(list(path_lengths.values()))][0]
+                    )
+        self.sensor_nodes   = sensor_nodes
