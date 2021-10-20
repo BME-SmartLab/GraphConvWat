cd9edf7e852c8c7d89501b338d56a986b2051354
ghajgato <ghajgato@hds.bme.hu>
2021-10-20 14:15:47 +0200

    pipe length and inverse weight added to edge props

--- utils/graph_utils.py
+++ utils/graph_utils.py
@@ -10,53 +10,62 @@ def get_nx_graph(wds, mode='binary'):
     if mode == 'binary':
         for pipe in wds.pipes:
             if (pipe.from_node.index in junc_list) and (pipe.to_node.index in junc_list):
-                G.add_edge(pipe.from_node.index, pipe.to_node.index, weight=1.)
+                G.add_edge(pipe.from_node.index, pipe.to_node.index, weight=1., length=pipe.length)
         for pump in wds.pumps:
             if (pump.from_node.index in junc_list) and (pump.to_node.index in junc_list):
-                G.add_edge(pump.from_node.index, pump.to_node.index, weight=1.)
+                G.add_edge(pump.from_node.index, pump.to_node.index, weight=1., length=0.)
         for valve in wds.valves:
             if (valve.from_node.index in junc_list) and (valve.to_node.index in junc_list):
-                G.add_edge(valve.from_node.index, valve.to_node.index, weight=1.)
+                G.add_edge(valve.from_node.index, valve.to_node.index, weight=1., length=0.)
     elif mode == 'weighted':
-        max_weight = 0
+        max_weight  = 0
+        max_iweight = 0
         for pipe in wds.pipes:
             if (pipe.from_node.index in junc_list) and (pipe.to_node.index in junc_list):
                 weight  = ((pipe.diameter*3.281)**4.871 * pipe.roughness**1.852) / (4.727*pipe.length*3.281)
-                G.add_edge(pipe.from_node.index, pipe.to_node.index, weight=weight)
+                iweight = weight**-1
+                G.add_edge(pipe.from_node.index, pipe.to_node.index,
+                        weight  = weight,
+                        iweight = iweight,
+                        length  = pipe.length
+                        )
                 if weight > max_weight:
                     max_weight = weight
+                if iweight > max_iweight:
+                    max_iweight = iweight
         for (_,_,d) in G.edges(data=True):
             d['weight'] /= max_weight
+            d['iweight'] /= max_iweight
         for pump in wds.pumps:
             if (pump.from_node.index in junc_list) and (pump.to_node.index in junc_list):
-                G.add_edge(pump.from_node.index, pump.to_node.index, weight=1.)
+                G.add_edge(pump.from_node.index, pump.to_node.index, weight=1., length=0.)
         for valve in wds.valves:
             if (valve.from_node.index in junc_list) and (valve.to_node.index in junc_list):
-                G.add_edge(valve.from_node.index, valve.to_node.index, weight=1.)
+                G.add_edge(valve.from_node.index, valve.to_node.index, weight=1., length=0.)
     elif mode == 'logarithmic':
         max_weight = 0
         for pipe in wds.pipes:
             if (pipe.from_node.index in junc_list) and (pipe.to_node.index in junc_list):
                 weight  = np.log10(((pipe.diameter*3.281)**4.871 * pipe.roughness**1.852) / (4.727*pipe.length*3.281))
-                G.add_edge(pipe.from_node.index, pipe.to_node.index, weight=float(weight))
+                G.add_edge(pipe.from_node.index, pipe.to_node.index, weight=float(weight), length=pipe.length)
                 if weight > max_weight:
                     max_weight = weight
         for (_,_,d) in G.edges(data=True):
             d['weight'] /= max_weight
         for pump in wds.pumps:
             if (pump.from_node.index in junc_list) and (pump.to_node.index in junc_list):
-                G.add_edge(pump.from_node.index, pump.to_node.index, weight=1.)
+                G.add_edge(pump.from_node.index, pump.to_node.index, weight=1., length=0.)
         for valve in wds.valves:
             if (valve.from_node.index in junc_list) and (valve.to_node.index in junc_list):
-                G.add_edge(valve.from_node.index, valve.to_node.index, weight=1.)
+                G.add_edge(valve.from_node.index, valve.to_node.index, weight=1., length=0.)
     elif mode == 'pruned':
         for pipe in wds.pipes:
             if (pipe.from_node.index in junc_list) and (pipe.to_node.index in junc_list):
-                G.add_edge(pipe.from_node.index, pipe.to_node.index, weight=0.)
+                G.add_edge(pipe.from_node.index, pipe.to_node.index, weight=0., length=pipe.length)
         for pump in wds.pumps:
             if (pump.from_node.index in junc_list) and (pump.to_node.index in junc_list):
-                G.add_edge(pump.from_node.index, pump.to_node.index, weight=0.)
+                G.add_edge(pump.from_node.index, pump.to_node.index, weight=0., length=0.)
         for valve in wds.valves:
             if (valve.from_node.index in junc_list) and (valve.to_node.index in junc_list):
-                G.add_edge(valve.from_node.index, valve.to_node.index, weight=0.)
+                G.add_edge(valve.from_node.index, valve.to_node.index, weight=0., length=0.)
     return G
