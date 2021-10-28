d60414fcffdda0966caba83827ff0196987b1e37
ghajgato <ghajgato@hds.bme.hu>
2021-10-28 15:59:34 +0200

    proper markers and colors

--- evaluation/plot_Taylor_diag_for_sensors.py
+++ evaluation/plot_Taylor_diag_for_sensors.py
@@ -37,20 +37,14 @@ args    = parser.parse_args()
 # DB loading
 # ----- ----- ----- ----- ----- -----
 df  = pd.read_csv(os.path.join('..', 'experiments', 'Taylor_metrics_processed.csv'), index_col=0)
-
-wds = args.wds
-std_ref = df.loc[
-    (df['wds'] == wds) &
-    (df['obs_rat'] == .05) &
-    (df['model'] == 'orig'), 'sigma_true'].tolist()[0]
-df  = df.loc[(df['tag'] == 'placement') & (df['wds'] == 'anytown')]
+df  = df.loc[(df['tag'] == 'placement') & (df['wds'] == args.wds)]
 
 # ----- ----- ----- ----- ----- -----
 # Plot assembly
 # ----- ----- ----- ----- ----- -----
 fig = plt.figure()
 dia = TaylorDiagram(
-        std_ref/std_ref,
+        1,
         fig     = fig,
         label   = 'reference',
         srange  = (args.smin, args.smax)
@@ -58,23 +52,28 @@ dia = TaylorDiagram(
 dia.samplePoints[0].set_color('r')
 dia.samplePoints[0].set_marker('P')
 cmap    = plt.get_cmap('Dark2')
-markers = ['o', 'x', '*', 's']
+markers = ['s', 'x', '*', '.']
 
-def add_samples(dia, key, color, marker):
-    for idx_dst, row in df.loc[df['placement'] == key].iterrows():
+def add_samples(dia, df, color, marker):
+    for idx_dst, row in df.iterrows():
         sigma   = row['sigma_pred'] / row['sigma_true']
         rho     = row['corr_coeff']
         dia.add_sample(sigma, rho,
             marker  = marker,
-            ms  = 7,
+            ms  = 9,
             ls  = '',
             mec = color,
             mew = 2,
             mfc = 'none',
             )
 
-add_samples(dia, 'dist', cmap(1), markers[0])
-add_samples(dia, 'hydrodist', cmap(2), markers[1])
+seeds   = [1, 8, 5266, 739, 88867]
+for i, seed in enumerate(seeds):
+    mask    = (df['placement'] == 'random') & (df['seed'] == seed)
+    add_samples(dia, df.loc[mask], cmap(i+4), markers[3])
+add_samples(dia, df.loc[df['placement'] == 'dist'], cmap(1), markers[0])
+add_samples(dia, df.loc[df['placement'] == 'hydrodist'], cmap(2), markers[1])
+add_samples(dia, df.loc[df['placement'] == 'hds'], cmap(3), markers[2])
 
 contours    = dia.add_contours(
                 levels      = 6,
