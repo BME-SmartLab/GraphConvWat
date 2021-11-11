5d5b00f8026bc20f2947f544f32513758316d20b
Gergely Hajgató <ghajgato@hds.bme.hu>
2021-11-11 11:55:14 +0100

    func call bugfix

--- batch_run.sh
+++ batch_run.sh
@@ -3,20 +3,20 @@ for idx in {1..3}
 do
 	for deploy in dist hydrodist hds
 	do
-		python train.py --wds anytown --obsrat 0.1 --adj binary --tag placement --runid $idx --deploy $deploy
-		python train.py --wds ctown --obsrat 0.05 --adj binary --tag placement --runid $idx --deploy $deploy
-		python train.py --wds richmond --obsrat 0.05 --adj binary --tag placement --runid $idx --deploy $deploy
+		python train.py --wds anytown --obsrat 0.1 --adj binary --tag placement --deploy $deploy --batch 200
+		python train.py --wds ctown --obsrat 0.05 --adj binary --tag placement --deploy $deploy --batch 120
+		python train.py --wds richmond --obsrat 0.05 --adj binary --tag placement --deploy $deploy --batch 50
 	done
 done
 for idx in {1..3}
 do
-	python train.py --wds anytown --obsrat 0.1 --adj binary --tag placement --runid $idx --deploy hds --aversion 1
-	python train.py --wds ctown --obsrat 0.05 --adj binary --tag placement --runid $idx --deploy hds --aversion 5
-	python train.py --wds richmond --obsrat 0.05 --adj binary --tag placement --runid $idx --deploy hds --aversion 7
+	python train.py --wds anytown --obsrat 0.1 --adj binary --tag placement --deploy hds --aversion 1 --batch 200
+	python train.py --wds ctown --obsrat 0.05 --adj binary --tag placement --deploy hds --aversion 5 --batch 120
+	python train.py --wds richmond --obsrat 0.05 --adj binary --tag placement --deploy hds --aversion 7 --batch 50
 done
 for idx in {1..15}
 do
-	python train.py --wds anytown --obsrat 0.1 --adj binary --tag placement --runid $idx --deploy random --deterministic
-	python train.py --wds ctown --obsrat 0.05 --adj binary --tag placement --runid $idx --deploy random --deterministic
-	python train.py --wds richmond --obsrat 0.05 --adj binary --tag placement --runid $idx --deploy random --deterministic
+	python train.py --wds anytown --obsrat 0.1 --adj binary --tag placement --deploy random --deterministic --batch 200
+	python train.py --wds ctown --obsrat 0.05 --adj binary --tag placement --deploy random --deterministic --batch 120
+	python train.py --wds richmond --obsrat 0.05 --adj binary --tag placement --deploy random --deterministic --batch 50
 done
